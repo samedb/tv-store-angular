ea51253d0ac030421957b6a78b053bf29f86c4b1
unknown <samed_97b@hotmail.com>
2020-08-02 16:15:38 +0200

    Prikazivanje slika i dodavanje u korpu u radjeno u product-page

--- src/app/pages/product-page/product-page.component.ts
+++ src/app/pages/product-page/product-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
 import { TV } from 'src/app/models/TV';
 import { ProductService } from 'src/app/services/product.service';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-product-page',
@@ -12,16 +13,26 @@ export class ProductPageComponent implements OnInit {
 
   tv: TV
   keys: string[]
+  selektovanaSlika: string
 
-  constructor(private route: ActivatedRoute, private productService: ProductService) { }
+  constructor(private route: ActivatedRoute, private productService: ProductService, private cartService: CartService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       const ean = params['ean']
       console.log(ean)
       this.tv = this.productService.getProducts().find(p => p.ean === ean)
-      this.keys = Object.keys(this.tv)
+      this.keys = Object.keys(this.tv).filter(k => k !== "slike")
+      this.selektovanaSlika = this.tv.slike[0]
     })
+  }
+
+  setSelektovanaSlika(slika: string): void {
+    this.selektovanaSlika = slika
+  }
+
+  addToCart(product: TV) {
+    this.cartService.addToCart(product)
   }
 
 }
